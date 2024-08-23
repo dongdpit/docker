eae13651a95e81e954b588bd891d6826c6b5322b
dongdpit <165973148+dongdpit@users.noreply.github.com>
2024-08-23 14:41:38 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -14,8 +14,8 @@ echo
 
 cat <<EOF | sudo tee /etc/environment
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-export http_proxy="http://10.26.2.55:8080"
-export https_proxy="http://10.26.2.55:8080"
+export http_proxy="http://"
+export https_proxy="http://"
 export no_proxy="localhost,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,169.254.0.0/16,127.0.0.0/8"
 EOF
 
@@ -33,8 +33,8 @@ mkdir -p /etc/systemd/system/docker.service.d
 
 cat <<EOT>> /etc/systemd/system/docker.service.d/http-proxy.conf
 [Service]
-Environment="HTTP_PROXY=10.26.2.55:8080"
-Environment="HTTPS_PROXY=10.26.2.55:8080"
+Environment="HTTP_PROXY="
+Environment="HTTPS_PROXY="
 Environment="NO_PROXY=10.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.168.0.0/16,127.0.0.0/8,localhost"
 EOT
 
